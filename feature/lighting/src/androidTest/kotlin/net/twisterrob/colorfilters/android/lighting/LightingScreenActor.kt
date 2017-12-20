7cbabc4051e39be23c970b91c5f084b7b749ff00
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2017-12-20 01:41:51 +0000

    [SVN] r2459 Android Color Filters in /Projects/Android Color Filters/ [REF] Split androidTest sources to modules (fake, because there's no way to depend properly) [ADD] Kotlin support to all modules (through new plugin) [MOD] Stricten Kotlin compilation

--- feature/lighting/src/androidTest/kotlin/net/twisterrob/colorfilters/android/lighting/LightingScreenActor.kt
+++ feature/lighting/src/androidTest/kotlin/net/twisterrob/colorfilters/android/lighting/LightingScreenActor.kt
@@ -4,7 +4,6 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
-import net.twisterrob.colorfilters.android.R
 import net.twisterrob.colorfilters.android.ScreenActor
 
 class LightingScreenActor : ScreenActor {
