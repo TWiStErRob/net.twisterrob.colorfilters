5f24be595ef8d291c2506122c39b2bd1647d6d91
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-07-10 00:14:12 +0000

    [SVN] r2585 Android Color Filters in /Projects/Android Color Filters/ [FIX] Add missing suppression

--- app/src/main/java/net/twisterrob/colorfilters/android/PreferencesActivity.java
+++ app/src/main/java/net/twisterrob/colorfilters/android/PreferencesActivity.java
@@ -6,6 +6,7 @@ import android.preference.PreferenceActivity;
 
 public class PreferencesActivity extends PreferenceActivity {
 	@Override
+	@SuppressWarnings("deprecation")
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
