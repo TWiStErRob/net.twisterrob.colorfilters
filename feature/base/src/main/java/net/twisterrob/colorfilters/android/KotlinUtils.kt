ad5c3049e314fa2a988af012c01594d415fd3a88
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-08-14 22:32:31 +0000

    [SVN] r2623 Android Color Filters in /Projects/Android Color Filters/ [FIX] Formatting crash due to not enough args

--- feature/base/src/main/java/net/twisterrob/colorfilters/android/KotlinUtils.kt
+++ feature/base/src/main/java/net/twisterrob/colorfilters/android/KotlinUtils.kt
@@ -1,4 +1,4 @@
 package net.twisterrob.colorfilters.android
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun String.formatRoot(vararg args: Any?) = this.format(java.util.Locale.ROOT, args)
+inline fun String.formatRoot(vararg args: Any?) = this.format(java.util.Locale.ROOT, *args)
