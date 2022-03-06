121f425e8334d2310b235394f1df2b01a5c53ff9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-03-06 11:17:54 +0000

    Move centralized setup to android conventions

--- buildSrc/src/main/kotlin/build.android-base.conventions.gradle.kts
+++ buildSrc/src/main/kotlin/build.android-base.conventions.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
+
 plugins {
 	id("net.twisterrob.kotlin")
 	id("net.twisterrob.quality")
@@ -11,5 +14,54 @@ tasks.withType<JavaCompile> {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 	kotlinOptions {
 		allWarningsAsErrors = true
+	}
+}
+
+@Suppress("UnstableApiUsage")
+project.plugins.withType<com.android.build.gradle.BasePlugin> plugin@{
+	val android = project.extensions["android"] as CommonExtension<*, *, *, *>
+	android.apply android@{
+		defaultConfig {
+			testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		}
+		lint {
+			warningsAsErrors = true
+			checkReleaseBuilds = false
+			// REPORT this is not working in AS
+			// workaround? java.nio.file.Files.createSymbolicLink in settings.gradle
+			lintConfig = rootProject.file("config/lint/lint.xml")
+			val cleanPath = project.path.substring(1).replace(':', '+')
+			baseline = rootProject.file("config/lint/baseline ${cleanPath}.xml")
+		}
+		if (this@plugin is com.android.build.gradle.LibraryPlugin) {
+			this@android as LibraryExtension
+			defaultConfig {
+				// Enable multidex for all libraries.
+				// This will transfer to androidTest apps in those libraries, but not the app.
+				multiDexEnabled = true
+			}
+			buildFeatures.buildConfig = project.path == ":feature:about"
+		}
+		val VERSION_KOTLIN: String by project.properties
+		configurations.all {
+			resolutionStrategy {
+				force("org.jetbrains.kotlin:kotlin-stdlib:${VERSION_KOTLIN}")
+				force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${VERSION_KOTLIN}")
+				exclude(group = "androidx.legacy")
+			}
+		}
+	}
+}
+
+// TODEL https://github.com/TWiStErRob/net.twisterrob.gradle/issues/214
+afterEvaluate {
+	val lint = project.tasks.withType<com.android.build.gradle.internal.lint.LintModelWriterTask>()
+	val ex = tasks.named("extractMinificationRules")
+	lint.configureEach { dependsOn(ex) }
+	if (project.path == ":app") {
+		afterEvaluate { // double-jump is required because this gets applied before the build plugin.
+			val min = tasks.named("generateReleaseMinificationRules")
+			lint.configureEach { dependsOn(min) }
+		}
 	}
 }
