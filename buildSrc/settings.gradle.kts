0de93bf058d48d584a593e96fae51cdb8f03a2a0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-04-02 19:27:42 +0100

    Use snapshot

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -9,6 +9,17 @@ dependencyResolutionManagement {
 			}
 		}
 		mavenCentral()
+		maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+			name = "Sonatype 01: SNAPSHOTs"
+			content {
+				includeVersionByRegex("""^net\.twisterrob\.gradle$""", ".*", """.*-SNAPSHOT$""")
+				includeVersionByRegex("""^net\.twisterrob\.gradle$""", ".*", """.*-\d{8}\.\d{6}-\d+$""")
+			}
+			mavenContent {
+				// This doesn't allow using specific snapshot, so using versionRegex above.
+				//snapshotsOnly()
+			}
+		}
 	}
 	versionCatalogs {
 		create("libs") {
