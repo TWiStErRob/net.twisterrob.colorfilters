c2d98e1c31188f1bb31570e04029000f8623dcba
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-30 22:28:06 +0000

    Try to ignore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -40,17 +40,6 @@ pluginManagement {
 				includeGroup("com.gradle.enterprise")
 			}
 		}
-		maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
-			name = "Sonatype 01: SNAPSHOTs"
-			content {
-				includeVersionByRegex("""^net\.twisterrob\.gradle$""", ".*", """.*-SNAPSHOT$""")
-				includeVersionByRegex("""^net\.twisterrob\.gradle$""", ".*", """.*-\d{8}\.\d{6}-\d+$""")
-			}
-			mavenContent {
-				// This doesn't allow using specific snapshot, so using versionRegex above.
-				//snapshotsOnly()
-			}
-		}
 	}
 }
 
