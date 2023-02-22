006d2be636e3367b14652de419fefa06baee7474
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-22 16:58:12 +0000

    Extract Detekt to separate plugin

--- gradle/plugins/src/main/kotlin/build.android-base.conventions.gradle.kts
+++ gradle/plugins/src/main/kotlin/build.android-base.conventions.gradle.kts
@@ -4,15 +4,13 @@ import com.android.build.api.dsl.LibraryDefaultConfig
 import com.android.build.api.dsl.LibraryExtension
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.internal.tasks.DexMergingTask
-import io.gitlab.arturbosch.detekt.Detekt
-import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 import net.twisterrob.gradle.android.androidComponents
 
 plugins {
 	id("kotlin-android")
 	id("kotlin-kapt")
 	id("net.twisterrob.quality")
-	id("io.gitlab.arturbosch.detekt")
+	id("net.twisterrob.colorfilters.build.detekt")
 }
 
 tasks.withType<JavaCompile> {
@@ -129,45 +127,6 @@ run {
 			}
 		}
 	}
-}
-
-detekt {
-	ignoreFailures = true
-	buildUponDefaultConfig = true
-	allRules = true
-	config = rootProject.files("config/detekt/detekt.yml")
-	baseline = rootProject.file("config/detekt/detekt-baseline-${project.name}.xml")
-	basePath = rootProject.projectDir.absolutePath
-
-	parallel = true
-
-	tasks.withType<Detekt>().configureEach {
-		// Target version of the generated JVM bytecode. It is used for type resolution.
-		jvmTarget = libs.versions.java.get()
-		reports {
-			html.required.set(true) // human
-			xml.required.set(true) // checkstyle
-			txt.required.set(true) // console
-			// https://sarifweb.azurewebsites.net
-			sarif.required.set(true) // Github Code Scanning
-		}
-	}
-}
-
-val detektReportMergeSarif = rootProject.tasks.named<ReportMergeTask>("detektReportMergeSarif")
-tasks.withType<Detekt>().configureEach {
-	finalizedBy(detektReportMergeSarif)
-}
-detektReportMergeSarif.configure {
-	input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
-}
-
-val detektReportMergeXml = rootProject.tasks.named<ReportMergeTask>("detektReportMergeXml")
-tasks.withType<Detekt>().configureEach {
-	finalizedBy(detektReportMergeXml)
-}
-detektReportMergeXml.configure {
-	input.from(tasks.withType<Detekt>().map { it.xmlReportFile })
 }
 
 // Prevent the following error by allowing only a few parallel executions of these tasks:
