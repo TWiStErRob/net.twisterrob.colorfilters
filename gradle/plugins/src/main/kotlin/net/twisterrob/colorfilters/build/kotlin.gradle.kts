ea7e9c3066500d76aae05e6bd2deb8aa11d48267
Róbert Papp <papp.robert.s@gmail.com>
2023-06-02 14:09:40 +0100

    Upgrade from Java 8 to Java 11 (#185)

--- gradle/plugins/src/main/kotlin/net/twisterrob/colorfilters/build/kotlin.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/colorfilters/build/kotlin.gradle.kts
@@ -1,5 +1,6 @@
 package net.twisterrob.colorfilters.build
 
+import net.twisterrob.colorfilters.build.dsl.android
 import net.twisterrob.colorfilters.build.dsl.libs
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -13,16 +14,37 @@ dependencies {
 	add("implementation", (libs.kotlin.stdlib.jdk8))
 }
 
+//<editor-fold desc="Java JVM Source/Target/Release/Toolchain Setup" defaultstate="collapsed">
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
 	compilerOptions {
 		jvmTarget.set(libs.versions.java.map(JvmTarget::fromTarget))
-		allWarningsAsErrors.set(true)
 	}
 }
 
 tasks.withType<JavaCompile>().configureEach {
 	sourceCompatibility = libs.versions.java.get()
 	targetCompatibility = libs.versions.java.get()
+}
+
+plugins.withId("com.android.base") {
+	android {
+		compileOptions {
+			sourceCompatibility = libs.versions.java.map(JavaVersion::toVersion).get()
+			targetCompatibility = libs.versions.java.map(JavaVersion::toVersion).get()
+		}
+	}
+}
+//</editor-fold>
+
+//<editor-fold desc="Strict Compilation" defaultstate="collapsed">
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+	compilerOptions {
+		allWarningsAsErrors.set(true)
+	}
+}
+
+tasks.withType<JavaCompile>().configureEach {
 	options.compilerArgs.add("-Xlint:all")
 	options.compilerArgs.add("-Werror")
 }
+//</editor-fold>
