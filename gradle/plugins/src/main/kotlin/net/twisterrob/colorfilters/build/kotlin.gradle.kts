a65f9d03156e41219313da9f788c3fcc57df92f4
Róbert Papp <papp.robert.s@gmail.com>
2025-06-29 15:35:33 +0200

    Global

--- gradle/plugins/src/main/kotlin/net/twisterrob/colorfilters/build/kotlin.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/colorfilters/build/kotlin.gradle.kts
@@ -40,11 +40,21 @@ plugins.withId("com.android.base") {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
 	compilerOptions {
 		allWarningsAsErrors.set(true)
+
 		// Workaround for https://youtrack.jetbrains.com/issue/KT-68400
 		// > Task :...:kaptGenerateStubsDebugUnitTestKotlin FAILED
 		// > w: Kapt currently doesn't support language version 2.0+. Falling back to 1.9.
 		// > e: warnings found and -Werror specified
 		freeCompilerArgs.add("-Xsuppress-version-warnings")
+
+                // Kotlin 2.2 started warning about this:
+                // > w: ... This annotation is currently applied to the value parameter only,
+                // > but in the future it will also be applied to field.	
+                // > - To opt in to applying to both value parameter and field,
+                // >   add '-Xannotation-default-target=param-property' to your compiler arguments.	
+                // > - To keep applying to the value parameter only, use the '@param:' annotation target.	
+                // > See https://youtrack.jetbrains.com/issue/KT-73255 for more details.
+                freeCompilerArgs.add("-Xannotation-default-target=param-property")
 	}
 }
 
