3241a7f87b9a534acf05f2d0d706d814269fc4fa
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-02-21 13:24:33 +0000

    old dsl

--- gradle/plugins/src/main/kotlin/net/twisterrob/colorfilters/build/kotlin.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/colorfilters/build/kotlin.gradle.kts
@@ -6,7 +6,7 @@ import net.twisterrob.colorfilters.build.dsl.libs
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-	//id("org.jetbrains.kotlin.android")
+	id("org.jetbrains.kotlin.android")
 }
 
 dependencies {
