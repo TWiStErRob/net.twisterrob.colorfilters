d828369392367229e19afc26633e4d7bb187fe8b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-07-18 00:05:52 +0100

    UnnecessaryFullyQualifiedName: Add import for Locale

--- feature/base/src/main/kotlin/net/twisterrob/colorfilters/android/KotlinUtils.kt
+++ feature/base/src/main/kotlin/net/twisterrob/colorfilters/android/KotlinUtils.kt
@@ -1,4 +1,7 @@
 package net.twisterrob.colorfilters.android
 
+import java.util.Locale
+
 @Suppress("NOTHING_TO_INLINE")
-inline fun String.formatRoot(vararg args: Any?) = this.format(java.util.Locale.ROOT, *args)
+inline fun String.formatRoot(vararg args: Any?) =
+	this.format(Locale.ROOT, *args)
